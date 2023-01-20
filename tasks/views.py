88515fc845db807f7c540c6f787b4c70f2dd127a
BeHappy <greenuran238@gmail.com>
2023-01-20 10:16:52 +0300

    Dynamic ser

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Task
 from .permissions import IsAuthorOrReadOnly 
 from .serializers import TaskSerializer
+from rest_framework.response import Response
 
 
 class TaskList(generics.ListCreateAPIView):
@@ -10,8 +11,16 @@ class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+    
+    
     def get_queryset(self):
         return super().get_queryset().filter(author=self.request.user or self.request.user.is_superuser)
+    
+    def list(self, request):
+        # Note the use of `get_queryset()` instead of `self.queryset`
+        queryset = self.get_queryset()
+        serializer = TaskSerializer(queryset, many=True, fields=('id', 'title', 'deadline'))
+        return Response(serializer.data)
 
 
 class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
